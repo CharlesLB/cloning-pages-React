b1faff2d75abfc766e19c78d2f2714c936533827
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-07-14 18:41:10 -0300

    :art: Global Styles

--- discord-clone/src/styles/GlobalStyles.ts
+++ discord-clone/src/styles/GlobalStyles.ts
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
     *{
@@ -24,12 +24,10 @@ export default createGlobalStyle`
     --quaternary: #292b2f;
     --quinary: #393d42;
     --senary: #828386;
-   
     --white: #fff;
     --gray: #8a8c90;
     --chat-input: rgb(64,68,75);
     --symbol: #74777a;
- 
     --notification: #f84a4b;
     --discord: #6e86d6;
     --mention-detail: #f9a839;
@@ -37,4 +35,4 @@ export default createGlobalStyle`
  
     --link: #5d80d6;
    }
-`
+`;
