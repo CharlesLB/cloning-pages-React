d7c5e55d3d88fda95ccc5e94be28076af303bf2c
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-07-14 18:41:26 -0300

    :chart_with_downwards_trend: App

--- discord-clone/src/App.tsx
+++ discord-clone/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
     <div className="App">
-
+        <Layout/>
+        <GlobalStyles/>
     </div>
   );
 }
