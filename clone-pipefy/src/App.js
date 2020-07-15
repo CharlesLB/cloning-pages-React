6d8733db4e5ca013a2ca8eddbad7a6dbf3b15161
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-07-15 14:09:53 -0300

    :apple: Drag And Drop

--- clone-pipefy/src/App.js
+++ clone-pipefy/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { DndProvider} from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 
 import GlobalStyles from './styles/globals';
 import Header from './components/Header';
@@ -6,11 +8,11 @@ import Board from './components/Board';
 
 function App() {
   return (
-    <div className="App">
+    <DndProvider backend={HTML5Backend}>
         <Header/>
         <Board/>
         <GlobalStyles/>
-    </div>
+    </DndProvider>
   );
 }
 
