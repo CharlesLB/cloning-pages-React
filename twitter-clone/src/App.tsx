2248dc4ded5220132b708af4a009818ae1724fca
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-07-15 10:14:30 -0300

    :computer: App

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="app">
+            <GlobalStyles />
+            <Layout />
+        </div>
+    );
 }
 
 export default App;
