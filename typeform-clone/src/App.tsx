4ff38d3242a6d98af7d2cb6412c3758ee4d2397c
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-10-04 09:38:04 -0300

    :art: First and Second

--- typeform-clone/src/App.tsx
+++ typeform-clone/src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Main } from './styles';
+import React from "react";
 
-import GlobalStyles from './styles/global';
+import { Main, Section, Sticky } from "./styles";
+
+import GlobalStyles from "./styles/global";
+import Header from "./components/Header";
+import FirstAndSecond from "./components/FirstAndSecond";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <GlobalStyles>
-        <Main>
+    React.useEffect(() => {
+        window.scrollTo({ top: 0 });
+    }, []);
 
-        </Main>
-    </GlobalStyles>
-  );
+    return (
+        <>
+            <GlobalStyles />
+
+            <Main style={{ height: "1610vh" }}>
+                <Header />
+
+                <Section style={{ height: "72.7%" }}>
+                    <FirstAndSecond />
+                </Section>
+
+                <Section style={{ height: "9.7%" }}>
+                    <Sticky className="third" />
+                </Section>
+
+                <Section style={{ height: "10.1%" }}>
+                    <Sticky className="fourth" />
+                </Section>
+
+                <Footer />
+            </Main>
+        </>
+    );
 }
 
 export default App;
